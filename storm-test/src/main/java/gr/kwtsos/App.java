602073a37e33176213576d5bc237462b0c489364
Kwtsos <57370817+kostasdj@users.noreply.github.com>
2021-03-23 22:19:51 +0200

    ΘΔΠκΔ_ασκ2

---
+++ storm-test/src/main/java/gr/kwtsos/App.java
@@ -0,0 +1,32 @@
+package gr.kwtsos;
+
+import org.apache.commons.lang.ObjectUtils.Null;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.topology.InputDeclarer;
+import org.apache.storm.topology.TopologyBuilder;
+
+public class App {
+    public static void main(String[] args){
+
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("IntegerSpout", new IntegerSpout());
+        InputDeclarer declarer = builder.setBolt("MultiplierBolt", new MultiplierBolt());
+        declarer.shuffleGrouping("IntegerSpout");
+        
+
+        Config config = new Config();
+        //config.setDebug(true);
+
+        LocalCluster cluster = null;
+        try{
+            cluster = new LocalCluster();
+            cluster.submitTopology("HelloTopology", config, builder.createTopology());
+            Thread.sleep(10000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            cluster.shutdown();
+        }
+    }
+}
